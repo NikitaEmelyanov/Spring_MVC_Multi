4faf992b2837899746877a75f7bf1df247710220
Emelyanov Nikita <mr.nikemelyanov@gmail.com>
2024-05-22 19:31:51 +0300

    Spring MVC Multi commit

---
+++ Les22_CRUD_Part2_Annotation_ModelAttribute/src/main/java/Les23/controllers/PeopleController.java
@@ -0,0 +1,44 @@
+package Les23.controllers;
+
+
+import Les23.dao.PersonDAO;
+import Les23.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()
+    public String index(@ModelAttribute("person") Person person) {
+        return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", personDAO.show(id));
+        return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
+    }
+}
